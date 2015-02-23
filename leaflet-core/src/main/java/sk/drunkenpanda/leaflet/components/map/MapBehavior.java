385176b2d4b619137a2efa9ca51dbbbe5edfd44e
Jan Ferko <julyloov@gmail.com>
2015-02-23 11:08:18 +0100

    Add abstractmaplayer

--- leaflet-core/src/main/java/sk/drunkenpanda/leaflet/components/map/MapBehavior.java
+++ leaflet-core/src/main/java/sk/drunkenpanda/leaflet/components/map/MapBehavior.java
@@ -1,7 +1,13 @@
 package sk.drunkenpanda.leaflet.components.map;
 
+import sk.drunkenpanda.leaflet.components.AbstractMapLayer;
+
 /**
  * @author Jan Ferko
  */
-public class MapBehavior {
+public class MapBehavior extends AbstractMapLayer<MapOptions> {
+
+    public MapBehavior(String id, String jsObjectName) {
+        super(id, jsObjectName);
+    }
 }
