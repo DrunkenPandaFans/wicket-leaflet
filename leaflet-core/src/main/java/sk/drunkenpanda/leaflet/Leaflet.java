5b6916b800bfcd5bb947a7234e08f025bf111d09
Jan Ferko <julyloov@gmail.com>
2015-07-21 10:39:15 +0200

    Update wicket-webjars version to 0.4.7

--- leaflet-core/src/main/java/sk/drunkenpanda/leaflet/Leaflet.java
+++ leaflet-core/src/main/java/sk/drunkenpanda/leaflet/Leaflet.java
@@ -16,76 +16,75 @@
 
 package sk.drunkenpanda.leaflet;
 
-import de.agilecoders.wicket.webjars.WebJarAssetLocator;
 import de.agilecoders.wicket.webjars.WicketWebjars;
 import org.apache.wicket.Application;
 import org.apache.wicket.MetaDataKey;
-import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.util.lang.Args;
 
 /**
  * Bootstrapping class of Wicket Leaflet.
- * It properly adds Leaflet resources to application based on {@link LeafletSettings}. 
+ * It properly adds Leaflet resources to application based on {@link LeafletSettings}.
  * If settings aren't provided default settings are used.
  * Wicket Leaflet uses WebJars for managing Leaflet.js resources.
  * If you don't want to use WebJars, you can use local or CDN resources by setting
  * all {@link org.apache.wicket.request.resource.ResourceReference}s int settings or by setting {@link LeafletSettings#useCDN() }
  * to {@code true}.
- * 
+ *
  * Library also appends resources automatically to all pages if {@link LeafletSettings#autoAppendResources() }
  * is {@code true}, otherwise client has to add resources to every component.
- * 
- * @author Jan Ferko 
+ *
+ * @author Jan Ferko
  */
 public final class Leaflet {
-    
+
     /** Meta key, that is used to identify stored settings in application. */
-    static final MetaDataKey<LeafletSettings> LEAFLET_SETTINGS_KEY = 
+    static final MetaDataKey<LeafletSettings> LEAFLET_SETTINGS_KEY =
             new MetaDataKey<LeafletSettings>() {};
-    
+
     private Leaflet() {
         throw new UnsupportedOperationException();
     }
-    
+
     /**
      * Installs Leaflet with default configuration to given application.
      * @param application application, which Leaflet should be bounded to.
      * @throws IllegalArgumentException if application is {@code null}.
-     * @see #install(org.apache.wicket.Application, sk.drunkenpanda.leaflet.LeafletSettings) 
+     * @see #install(org.apache.wicket.protocol.http.WebApplication, sk.drunkenpanda.leaflet.LeafletSettings)
      */
-    public static void install(Application application) {        
+    public static void install(WebApplication application) {
         install(application, null);
     }
-    
+
     /**
      * Install Leaflet with given settings to application.
      * If application already has Leaflet installed, it ignores new settings.
-     * 
+     *
      * @param application application, which Leaflet should be bounded to.
      * @param settings custom settings, which are used to initialized library
      * @throws IllegalArgumentException if application is {@code null}.
      */
-    public static void install(Application application, LeafletSettings settings) {
+    public static void install(WebApplication application, LeafletSettings settings) {
         // install Leaflet.js with given configuration
         Args.notNull(application, "application");
-        
+
         if (application.getMetaData(LEAFLET_SETTINGS_KEY) == null) {
             LeafletSettings settingsOrDefault = settings != null ? settings : new DefaultLeafletSettings();
             application.setMetaData(LEAFLET_SETTINGS_KEY, settingsOrDefault);
-            
+
             if (settingsOrDefault.autoAppendResources()) {
                 application.getComponentInstantiationListeners().add(new LeafletResourceAppender());
             }
-            
+
             if (settingsOrDefault.useWebJars()) {
                 WicketWebjars.install(application);
             }
         }
     }
-    
+
     /**
-     * Returns Leaflet settings of given application.     
-     * 
+     * Returns Leaflet settings of given application.
+     *
      * @param application application, which setting are retrieved
      * @return leaflet settings of application
      * @throws IllegalArgumentException if application is {@code null}
@@ -95,17 +94,17 @@ public final class Leaflet {
         // get settings by MetaKey
         Args.notNull(application, "application");
         LeafletSettings settings = application.getMetaData(LEAFLET_SETTINGS_KEY);
-        
+
         if (settings == null) {
             throw new IllegalStateException("Leaflets aren't installed in application [" + application.getName() + "].");
         }
-        
+
         return settings;
     }
-    
+
     /**
      * Returns settings of application, that belongs to current thread.
-     * 
+     *
      * @return settings of application in current thread
      * @throws IllegalStateException if there isn't any application in current thread
      */
@@ -114,8 +113,8 @@ public final class Leaflet {
         if (Application.exists()) {
             Application app = Application.get();
             return getSettings(app);
-        } 
+        }
         throw new IllegalStateException("Application was not found in current thread");
-    }        
+    }
 
 }
